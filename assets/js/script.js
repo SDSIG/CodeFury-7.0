d41c50e35d7b7da0818235b1148b9c388868e765
shr968 <shreyanayakb.261104@gmail.com>
2024-08-10 08:39:00 +0530

    stopped the countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,26 +8,26 @@ faqs.forEach(faq=>{
     })
 })
 // js for countdown
-let daysItem = document.querySelector("#days")
-let hoursItem = document.querySelector("#hours");
-let minItem = document.querySelector("#min");
-let secItem = document.querySelector("#sec")
+// let daysItem = document.querySelector("#days")
+// let hoursItem = document.querySelector("#hours");
+// let minItem = document.querySelector("#min");
+// let secItem = document.querySelector("#sec")
 
-let countDown = ()=>{
-    let futureDate = new Date("10 August 2024");
-    let currentDate = new Date();
-    let myDate = futureDate - currentDate;
-    let days = Math.floor(myDate/1000/60/60/24);
-    let hours = Math.floor(myDate/1000/60/60) % 24;
-    let min = Math.floor(myDate/1000/60)%60;
-    let sec = Math.floor(myDate/1000)%60;
-    daysItem.innerHTML=days;
-    hoursItem.innerHTML=hours;
-    minItem.innerHTML=min;
-    secItem.innerHTML=sec;
-}
-countDown();
-setInterval(countDown,1000);
+// let countDown = ()=>{
+//     let futureDate = new Date("10 August 2024");
+//     let currentDate = new Date();
+//     let myDate = futureDate - currentDate;
+//     let days = Math.floor(myDate/1000/60/60/24);
+//     let hours = Math.floor(myDate/1000/60/60) % 24;
+//     let min = Math.floor(myDate/1000/60)%60;
+//     let sec = Math.floor(myDate/1000)%60;
+//     daysItem.innerHTML=days;
+//     hoursItem.innerHTML=hours;
+//     minItem.innerHTML=min;
+//     secItem.innerHTML=sec;
+// }
+// countDown();
+// setInterval(countDown,1000);
 
 /**
  * add event on multiple elements
